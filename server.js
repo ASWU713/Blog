9f10594f8fb55a56d635a2bebaedb12c23e39fe1
Andrew Wu <59518876+ASWU713@users.noreply.github.com>
2021-08-15 10:46:37 -0500

    Pages connected to Mongoose

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles.js')
 const app = express();
 
+const url = 'mongodb://localhost/Blog';
+mongoose
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log( 'Database Connected' ))
+    .catch(err => console.log( err));
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter);
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) =>{
-    const articles = [{
-        title: 'Test Article', 
-        createdAt: new Date(),
-        description: 'Test Desc'
-    }, 
-    {
-        title: 'Test Article2', 
-        createdAt: new Date(),
-        description: 'Test Desc2'
-    }]
+app.get('/', async (req, res) =>{
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    
     res.render('articles/index.ejs', {articles: articles});
 });
 
+app.use('/articles', articleRouter);
 app.listen(5000);
