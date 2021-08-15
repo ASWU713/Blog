bc0918c039cc8ad90aa0461bbc84c3f266c8c155
Andrew Wu <59518876+ASWU713@users.noreply.github.com>
2021-08-15 15:27:19 -0500

    Working Blog

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article')
-const articleRouter = require('./routes/articles.js')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express();
 
 const url = 'mongodb://localhost/Blog';
+
 mongoose
     .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => console.log( 'Database Connected' ))
@@ -13,13 +15,11 @@ mongoose
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) =>{
-    const articles = await Article.find().sort({
-        createdAt: 'desc'
-    })
-    
-    res.render('articles/index.ejs', {articles: articles});
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index.ejs', {articles: articles}) 
 });
 
 app.use('/articles', articleRouter);
